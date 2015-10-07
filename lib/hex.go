c7a29b06c5cdf4172a74c18422724202a00c949e
Phil DiMarco <plantlife26@gmail.com>
2015-10-07 18:45:01 -0400

    almost working

--- lib/hex.go
+++ lib/hex.go
@@ -10,8 +10,8 @@ type ConnectionMap map[*Dot]*Connection
 type Dot struct {
 	DotID       string
 	Connections []ConnectionInfo
-	Connection
-	M ConnectionMap
+	M           ConnectionMap
+	full        bool
 }
 
 type Connection struct {
@@ -26,37 +26,40 @@ type ConnectionInfo struct {
 }
 
 func main() {
-	aDot := createEmptyDot("myDot")
-	aDot2 := createEmptyDot("myDot2")
-	aDot3 := createEmptyDot("myDot3")
+		a := MakeHex(3, 50)
 
-	addConnection(aDot, aDot2, 5, "c1")
-	addConnection(aDot3, aDot, 2, "c2")
-	addConnection(aDot2, aDot3, 3, "c3")
+		for i := range a{
+			fmt.Println(a[i].PrintDot()
+		}
 
-	fmt.Printf("%d", getConnection(aDot, aDot.Connections[0].connectionID).Distance)
-
-	aDot.PrintDot()
-	aDot2.PrintDot()
-	aDot3.PrintDot()
 }
 
 func createEmptyDot(s string) *Dot {
 	dot := new(Dot)
 	dot.DotID = s
 	dot.M = make(ConnectionMap)
+	dot.full = false
 	return dot
 }
 
-func addConnection(d, other *Dot, distance int, id string) {
+func (d *Dot) addConnection(other *Dot, distance int, id string) {
 
-	a := [2]*Dot{d, other}
-	c := &Connection{a, distance, id}
-	d.M[other] = c
-	other.M[d] = c
+	if _, ok := d.M[other]; !ok {
+		if len(d.Connections) < 6 {
+			a := [2]*Dot{d, other}
+			c := &Connection{a, distance, id}
+			d.M[other] = c
+			other.M[d] = c
 
-	d.Connections = append(d.Connections, ConnectionInfo{id, other.DotID})
-	other.Connections = append(other.Connections, ConnectionInfo{id, d.DotID})
+			d.Connections = append(d.Connections, ConnectionInfo{id, other.DotID})
+			other.Connections = append(other.Connections, ConnectionInfo{id, d.DotID})
+		} else {
+			d.full = true
+			fmt.Println("no room!")
+		}
+	} else {
+		fmt.Println("Already connected")
+	}
 }
 
 func getConnection(d *Dot, id string) *Connection {
@@ -70,10 +73,40 @@ func getConnection(d *Dot, id string) *Connection {
 	return a
 }
 
+func getDot(d *Dot, id string) *Dot {
+	for value := range d.M {
+		if value.DotID == id {
+			return value
+		}
+	}
+	a := new(Dot)
+	a.DotID = "nil"
+	return a
+}
+
+func MakeHex(distance, size int) []*Dot {
+	var dots [size]*Dot
+	for count := range dots {
+		dots[count] = createEmptyDot("i" + strconv.Itoa(count))
+	}
+	for count := range Dots{
+		dots[count] = fillDot(count, distance, dots)
+	}
+}
+
+func fillDot(i, distance int, dots []*Dot) *Dot {
+	for !dot.full {
+		count := 1
+		dots[i].addConnection(dots[i+count], distance, "c"+strconv.Itoa(i)+":"+strconv.Itoa(count))
+		count++
+	}
+}
+
+
 func (d *Dot) PrintDot() {
 	s := ""
 	for x := range d.Connections {
-		s = s + "\n	Name: " + d.Connections[x].ConnectionDot + " Distance: " + strconv.Itoa(getConnection(d, d.Connections[x].connectionID).Distance)
+		s = s + "\n	Name: " + d.Connections[x].ConnectionDot + " Distance: " + strconv.Itoa(getConnection(d, d.Connections[x].connectionID).Distance) //how to find distance with map?
 	}
 	fmt.Println("\nDot id: " + d.DotID + "\nConnections: " + s)
 }
